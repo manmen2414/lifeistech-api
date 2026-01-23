c1c536099089db93696daf1af7c5f11b44a257d8
manmen2414 <inotitokokoro@gmail.com>
2026-01-24 00:55:13 +0900

    add: export index

---
+++ src/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ...require("./chapter"),
+  ...require("./course"),
+  ...require("./errors"),
+  ...require("./lessongroup"),
+  ...require("./page"),
+  ...require("./pageComponents"),
+  ...require("./user"),
+  ...require("./util"),
+};
