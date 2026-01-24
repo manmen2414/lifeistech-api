8036418e30c00287e1446ebc2e94f12214605577
manmen2414 <inotitokokoro@gmail.com>
2026-01-24 14:28:01 +0000

    Generated: Library and d.ts files

--- dist/types/src/types/page.d.ts
+++ dist/types/src/types/page.d.ts
@@ -1,2 +1,13 @@
-type FileMinetypes = "text/html"|"text/css"|"application/javascript"
-export {FileMinetypes}
+type FileMinetypes = "text/html" | "text/css" | "application/javascript";
+type RenameReturns = Promise<
+  | { successed: true }
+  | {
+      successed: false;
+      error: {
+        /**タイトルが未入力。 */
+        emptyTitle: boolean;
+      };
+    }
+>;
+
+export { FileMinetypes, RenameReturns };
