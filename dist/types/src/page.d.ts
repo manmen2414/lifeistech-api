3c075f213cb06a767b6da312e4a325c653250e24
mameeenn <102225383+manmen2414@users.noreply.github.com>
2026-01-24 01:19:26 +0000

    Generated: Library and d.ts files

---
+++ dist/types/src/page.d.ts
@@ -0,0 +1,92 @@
+export type Loadable<T> = import("./types/util.d.ts").Loadable<T>;
+export type UserBase = import("./user.js").UserBase;
+/**
+ * @implements {Loadable<Page>}
+ */
+export class PageBase implements Loadable<Page> {
+    /**
+     * @param {number} id
+     * @param {string} name
+     * @param {UserBase} user
+     */
+    constructor(id: number, name: string, user: UserBase);
+    /**@type {Page|null} */
+    loaded: Page | null;
+    id: number;
+    name: string;
+    user: import("./user.js").UserBase;
+    load(): Promise<Page>;
+    getLoaded(): Promise<Page>;
+    /**@param {PageFile[]} files  */
+    save(files: PageFile[]): Promise<{
+        successed: boolean;
+        errorAt: {
+            /**@type {PageFile?} */
+            file: PageFile | null;
+            /**@type {string?} */
+            property: string | null;
+        };
+    }>;
+    /**@param {PageFile[]} files  */
+    applyFiles(files: PageFile[]): Promise<{
+        successed: boolean;
+        errorAt: {
+            /**@type {PageFile?} */
+            file: PageFile | null;
+            /**@type {string?} */
+            property: string | null;
+        };
+    }>;
+    /**
+     * @param {string} name
+     * @param {"html"|"css"|"js"} extension
+     * @returns {Promise<boolean|{error:string}>}
+     */
+    addFile(name: string, extension: "html" | "css" | "js"): Promise<boolean | {
+        error: string;
+    }>;
+    /**@param {boolean} formatToDataUrl  */
+    getZipB64(formatToDataUrl: boolean): Promise<string>;
+    /**
+     * @param {string} base64
+     * @param {string} name
+     */
+    uploadImage(name: string, base64: string): Promise<void>;
+}
+export class Page extends PageBase {
+    /**
+     * @param {number} id
+     * @param {UserBase} user
+     */
+    static Load(id: number, user: UserBase): Promise<Page>;
+    /**@param {PageBase} pageBase  */
+    constructor(pageBase: PageBase);
+    loaded: this;
+    /**
+     * @type {string}
+     * index.htmlへのアクセスリンク。
+     */
+    previewUrl: string;
+    /**@type {PageDataTable[]} データベースのリスト。*/
+    dataTables: PageDataTable[];
+    /**@type {PageFile[]} ファイルリスト。画像は含まれない。*/
+    files: PageFile[];
+    /**@type {PageImage[]} ユーザーが追加した画像リスト。プリセットの画像は含まれない。*/
+    images: PageImage[];
+    /**@type {PageImage[]} テンプレートに存在している画像リスト。ユーザー画像は含まれない。*/
+    presetImages: PageImage[];
+    load(): Promise<this>;
+    isLogining: boolean | undefined;
+    save(files?: PageFile[]): Promise<{
+        successed: boolean;
+        errorAt: {
+            /**@type {PageFile?} */
+            file: PageFile | null;
+            /**@type {string?} */
+            property: string | null;
+        };
+    }>;
+}
+import { PageFile } from "./pageComponents.js";
+import { PageDataTable } from "./pageComponents.js";
+import { PageImage } from "./pageComponents.js";
---
+++ dist/types/src/types/page.d.ts
@@ -0,0 +1,2 @@
+type FileMinetypes = "text/html"|"text/css"|"application/javascript"
+export {FileMinetypes}
